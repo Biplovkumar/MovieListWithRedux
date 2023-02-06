41a0167e501ee1319249f18e0beb3eebacac3778
Biplov Kumar <biplov.kumar@nagarro.com>
2023-02-06 08:33:40 +0530

    Done assignment with change language, login and Movie screen and little bit test cases

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,21 @@
 /**
  * @format
  */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
+import {render} from '@testing-library/react-native';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+
+jest.mock('../src/navigations/RootNavigator', () => 'RootNavigator');
+jest.mock('../src/utils/SharedPreferences', () => ({
+  get: jest.fn(() => Promise.resolve(null)),
+  save: jest.fn(),
+}));
 
 it('renders correctly', () => {
   renderer.create(<App />);
 });
+
